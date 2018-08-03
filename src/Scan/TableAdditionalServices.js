4acc77b8d7070871e54ba610e40fd6e6e2c0df98
i.muhin <i.muhin@cdek.ru>
2018-08-03 16:34:20 +0300

    Доп услуги

--- src/Scan/TableAdditionalServices.js
+++ src/Scan/TableAdditionalServices.js
@@ -1,8 +1,9 @@
 import React, {Fragment} from 'react';
 
 import {connector} from "../store/utils/connector";
-import {Button, Col, FormGroup, Input, Label, Table} from "reactstrap";
+import {Col, FormGroup, Input, Label, Table} from "reactstrap";
 import {getCalculationAndAdditionalServices} from "./serviceScan";
+import CurrencyInput from 'react-currency-input';
 
 const TableAdditionalServices = ({state, dispatch}) =>
     (
@@ -23,22 +24,42 @@ const TableAdditionalServices = ({state, dispatch}) =>
                             <tr key={idx}>
                                 <td>
                                     {
-                                        item.params && Array.isArray(item.params) && item.params.length > 0 ?
+                                        item.params && Array.isArray(item.params) && item.params.length > 0 && item.params.filter(item => item.type === 'list').length === 0 ?
                                             (
-                                                <div />
+                                                <FormGroup check>
+                                                    <Input type="checkbox"
+                                                           disabled={true}
+                                                           checked={item.select}
+                                                           onChange={() => {
+                                                           }}
+                                                    />
+                                                </FormGroup>
                                             )
                                             :
-                                            <FormGroup check>
-                                                <Input type="checkbox"
-                                                       checked={item.select}
-                                                       onChange={
-                                                           () => {
-                                                               item.select = !item.select;
-                                                               getCalculationAndAdditionalServices({state, dispatch}, true);
+                                            (
+                                                <FormGroup check>
+                                                    <Input type="checkbox"
+                                                           checked={item.select}
+                                                           onChange={
+                                                               () => {
+                                                                   item.select = !item.select;
+                                                                   if (item.params && Array.isArray(item.params) && item.params.filter( item1 => item1.type === 'list' ).length !== 0 ) {
+                                                                       if (item.params[0].values && Array.isArray(item.params[0].values) && item.params[0].values.length > 0) {
+                                                                           if (!item.params[0].value) {
+                                                                               item.params[0].value = item.params[0].values[0].code;
+                                                                               dispatch.changeParamsAdditionalServicesInput(state.scanReducer.order.services.additionalServices);
+                                                                           }
+                                                                       }
+                                                                   }
+                                                                   getCalculationAndAdditionalServices({
+                                                                       state,
+                                                                       dispatch
+                                                                   }, true);
+                                                               }
                                                            }
-                                                       }
-                                                />
-                                            </FormGroup>
+                                                    />
+                                                </FormGroup>
+                                            )
                                     }
                                 </td>
                                 <td>{item.name}</td>
@@ -48,31 +69,123 @@ const TableAdditionalServices = ({state, dispatch}) =>
                                             (
                                                 item.params.map(
                                                     (item1, idx1) => (
-                                                        <FormGroup key={idx1} row style={ {'margin-bottom': '0px'} }>
+                                                        <FormGroup key={idx1} row style={{'margin-bottom': '0px'}}>
                                                             <Label sm={5}>{item1.description}:</Label>
                                                             <Col sm={7}>
-                                                                { item1.type }
-                                                                <Input type="text"
-                                                                       bsSize={'sm'}
-                                                                       placeholder={item1.description}
-                                                                       value={item1.value || ''}
-                                                                       onChange={
-                                                                           (e) => {
-                                                                               item1.value = e.target.value? +e.target.value : null;
-                                                                               dispatch.changeParamsAdditionalServicesInput(state.scanReducer.order.services.additionalServices);
-                                                                           }
-                                                                       }
-                                                                       onBlur={
-                                                                           () => {
-                                                                               if ( item1.value && ('' + item1.value).trim() ) {
-                                                                                   item.select = true;
-                                                                               } else {
-                                                                                   item.select = false;
+                                                                {
+                                                                    item1.type === 'money' ? (
+                                                                        <CurrencyInput
+                                                                            decimalSeparator={','}
+                                                                            thousandSeparator={''}
+                                                                            precision={2}
+                                                                            allowEmpty={true}
+                                                                            // suffix=" rub"
+                                                                            // prefix="$ "
+                                                                            className={'form-control-sm form-control'}
+                                                                            value={item1.value}
+                                                                            onChangeEvent={
+                                                                                (event, maskedvalue, floatvalue) => {
+                                                                                    item1.value = floatvalue;
+                                                                                    dispatch.changeParamsAdditionalServicesInput(state.scanReducer.order.services.additionalServices);
+                                                                                }
+                                                                            }
+                                                                            onBlur={
+                                                                                () => {
+                                                                                    item.select = !!item1.value;
+                                                                                    getCalculationAndAdditionalServices({
+                                                                                        state,
+                                                                                        dispatch
+                                                                                    }, true);
+                                                                                }
+                                                                            }
+                                                                        />
+                                                                    ) : (<div/>)
+                                                                }
+                                                                {
+                                                                    item1.type === 'integer' ? (
+                                                                        <CurrencyInput
+                                                                            decimalSeparator={','}
+                                                                            thousandSeparator={''}
+                                                                            precision={'0'}
+                                                                            allowEmpty={true}
+                                                                            // suffix=" rub"
+                                                                            // prefix="$ "
+                                                                            className={'form-control-sm form-control'}
+                                                                            value={item1.value}
+                                                                            onChangeEvent={
+                                                                                (event, maskedvalue, floatvalue) => {
+                                                                                    item1.value = floatvalue;
+                                                                                    dispatch.changeParamsAdditionalServicesInput(state.scanReducer.order.services.additionalServices);
+                                                                                }
+                                                                            }
+                                                                            onBlur={
+                                                                                () => {
+                                                                                    item.select = !!item1.value;
+                                                                                    getCalculationAndAdditionalServices({
+                                                                                        state,
+                                                                                        dispatch
+                                                                                    }, true);
+                                                                                }
+                                                                            }
+                                                                        />
+                                                                    ) : (<div/>)
+                                                                }
+                                                                {
+                                                                    item1.type === 'string' ? (
+                                                                        <Input type="text"
+                                                                               bsSize={'sm'}
+                                                                               placeholder={item1.description}
+                                                                               value={item1.value || ''}
+                                                                               onChange={
+                                                                                   (e) => {
+                                                                                       item1.value = e.target.value;
+                                                                                       dispatch.changeParamsAdditionalServicesInput(state.scanReducer.order.services.additionalServices);
+                                                                                   }
                                                                                }
-                                                                               getCalculationAndAdditionalServices({state, dispatch}, true);
-                                                                           }
-                                                                       }
-                                                                />
+                                                                               onBlur={
+                                                                                   () => {
+                                                                                       if (item1.value && ('' + item1.value).trim()) {
+                                                                                           item.select = true;
+                                                                                       } else {
+                                                                                           item.select = false;
+                                                                                       }
+                                                                                       getCalculationAndAdditionalServices({
+                                                                                           state,
+                                                                                           dispatch
+                                                                                       }, true);
+                                                                                   }
+                                                                               }
+                                                                        />
+                                                                    ) : (<div/>)
+                                                                }
+                                                                {
+                                                                    item1.type === 'list' ? (
+                                                                        <div>
+                                                                            <Input bsSize={'sm'} type="select"
+                                                                                   value={item1.value}
+                                                                                   onChange={
+                                                                                       (e) => {
+                                                                                           item1.value = e.target.value;
+                                                                                           dispatch.changeParamsAdditionalServicesInput(state.scanReducer.order.services.additionalServices);
+                                                                                           getCalculationAndAdditionalServices({
+                                                                                               state,
+                                                                                               dispatch
+                                                                                           }, true);
+                                                                                       }
+                                                                                   }
+                                                                            >
+                                                                                {
+                                                                                    (item1.values || []).map(
+                                                                                        (item, idx) => (
+                                                                                            <option key={idx}
+                                                                                                    value={item.code}>{item.name}</option>
+                                                                                        )
+                                                                                    )
+                                                                                }
+                                                                            </Input>
+                                                                        </div>
+                                                                    ) : (<div/>)
+                                                                }
                                                             </Col>
                                                         </FormGroup>
                                                     )
